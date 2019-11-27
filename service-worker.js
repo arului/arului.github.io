2712d9cd19762679adc6c1caa7a0bfa77ec44ad7
arului <58032678+arului@users.noreply.github.com>
2019-11-27 16:38:56 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,11 @@ var cacheName = 'v1';
 var cacheFiles = [
 	'./',
 	'./index.html',
-	'./js/app.js',
-	'./css/reset.css',
-	'./css/style.css',
-	'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic'
+	'./js/main.js',
+	'./css/main.css',
+	'./css/normalize.css',
+	'https://code.jquery.com/jquery-3.4.1.min.js',
+	'https://fonts.googleapis.com/css?family=Anton|Roboto'
 ]
 
 
