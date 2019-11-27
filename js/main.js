489f635265d96b25279cc62aa380bc1bb3c30aed
arului <58032678+arului@users.noreply.github.com>
2019-11-27 16:33:57 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,7 +46,16 @@ $(document).ready(function() {
     }, 750 * (idx + 1));
   });
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js');
-  }
+
+  navigator.serviceWorker
+    .register('./service-worker.js', { scope: './' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+
+}
 });
 
