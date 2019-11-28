4a4f73dfc7781ed098f27e1e6cf5b5460f863eb4
arului <58032678+arului@users.noreply.github.com>
2019-11-28 11:25:48 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,13 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
 // Set a name for the current cache
 var cacheName = 'v1'; 
 
