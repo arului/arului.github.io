002cb79341ecb37091b1b9a4c15584d82df614ec
arului <58032678+arului@users.noreply.github.com>
2019-11-28 14:25:38 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,53 +4,60 @@ importScripts(
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
-	workbox.routing.registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'google-fonts-stylesheets',
-  }),
-);
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "google-fonts-stylesheets"
+    })
+  );
+  workbox.routing.registerRoute(
+    /^https:\/\/maxcdn\.bootstrapcdn\.com/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: "maxcdn-bootstrapcdn-stylesheets"
+    })
+  );
 
-// Cache the Google Fonts webfont files with a cache first strategy for 1 year.
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.gstatic\.com/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'google-fonts-webfonts',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      }),
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-      }),
-    ],
-  }),
-);
+  // Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com/,
+    workbox.strategies.cacheFirst({
+      cacheName: "google-fonts-webfonts",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365
+        })
+      ]
+    })
+  );
+  
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-"use strict";
+("use strict");
 
-console.log('WORKER: executing.');
+console.log("WORKER: executing.");
 
 /* A version number is useful when updating the worker logic,
    allowing you to remove outdated cache entries during the update.
 */
-var version = 'precache-v1::';
+var version = "precache-v1::";
 
 /* These resources will be downloaded and cached by the service worker
    during the installation process. If any resource fails to be downloaded,
    then the service worker won't be installed either.
 */
 var offlineFundamentals = [
-  'index.html',
-  './',
-	'./js/main.js',
-	'./css/main.css',
-	'./css/normalize.css',
-	'https://code.jquery.com/jquery-3.4.1.min.js',
-	'https://fonts.googleapis.com/css?family=Anton|Roboto'
+  "index.html",
+  "./",
+  "./js/main.js",
+  "./css/main.css",
+  "./css/normalize.css",
+  "https://code.jquery.com/jquery-3.4.1.min.js",
+  "https://fonts.googleapis.com/css?family=Anton|Roboto"
 ];
 
 /* The install event fires when the service worker is first installed.
@@ -58,29 +65,30 @@ var offlineFundamentals = [
    files while visitors are offline.
 */
 self.addEventListener("install", function(event) {
-  console.log('WORKER: install event in progress.');
+  console.log("WORKER: install event in progress.");
   /* Using event.waitUntil(p) blocks the installation process on the provided
-     promise. If the promise is rejected, the service worker won't be installed.
-  */
+       promise. If the promise is rejected, the service worker won't be installed.
+    */
   event.waitUntil(
     /* The caches built-in is a promise-based API that helps you cache responses,
-       as well as finding and deleting them.
-    */
+               as well as finding and deleting them.
+            */
     caches
       /* You can open a cache by name, and this method returns a promise. We use
-         a versioned cache name here so that we can remove old cache entries in
-         one fell swoop later, when phasing out an older service worker.
-      */
-      .open(version + 'fundamentals')
+               a versioned cache name here so that we can remove old cache entries in
+               one fell swoop later, when phasing out an older service worker.
+            */
+      .open(version + "fundamentals")
       .then(function(cache) {
         /* After the cache is opened, we can fill it with the offline fundamentals.
-           The method below will add all resources in `offlineFundamentals` to the
-           cache, after making requests for them.
-        */
+                           The method below will add all resources in `offlineFundamentals` to the
+                           cache, after making requests for them.
+                        */
         return cache.addAll(offlineFundamentals);
-      }).then(self.skipWaiting())
+      })
+      .then(self.skipWaiting())
       .then(function() {
-        console.log('WORKER: install completed');
+        console.log("WORKER: install completed");
       })
   );
 });
@@ -91,38 +99,42 @@ self.addEventListener("install", function(event) {
    CSS resources, fonts, any images, etc.
 */
 self.addEventListener("fetch", function(event) {
-  console.log('WORKER: fetch event in progress.');
+  console.log("WORKER: fetch event in progress.");
 
   /* We should only cache GET requests, and deal with the rest of method in the
-     client-side, by handling failed POST,PUT,PATCH,etc. requests.
-  */
-  if (event.request.method !== 'GET') {
-    /* If we don't block the event as shown below, then the request will go to
-       the network as usual.
+       client-side, by handling failed POST,PUT,PATCH,etc. requests.
     */
-    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+  if (event.request.method !== "GET") {
+    /* If we don't block the event as shown below, then the request will go to
+           the network as usual.
+        */
+    console.log(
+      "WORKER: fetch event ignored.",
+      event.request.method,
+      event.request.url
+    );
     return;
   }
   /* Similar to event.waitUntil in that it blocks the fetch event on a promise.
-     Fulfillment result will be used as the response, and rejection will end in a
-     HTTP response indicating failure.
-  */
+       Fulfillment result will be used as the response, and rejection will end in a
+       HTTP response indicating failure.
+    */
   event.respondWith(
     caches
       /* This method returns a promise that resolves to a cache entry matching
-         the request. Once the promise is settled, we can then provide a response
-         to the fetch request.
-      */
+               the request. Once the promise is settled, we can then provide a response
+               to the fetch request.
+            */
       .match(event.request)
       .then(function(cached) {
         /* Even if the response is in our cache, we go to the network as well.
-           This pattern is known for producing "eventually fresh" responses,
-           where we return cached responses immediately, and meanwhile pull
-           a network response and store that in the cache.
+                           This pattern is known for producing "eventually fresh" responses,
+                           where we return cached responses immediately, and meanwhile pull
+                           a network response and store that in the cache.
 
-           Read more:
-           https://ponyfoo.com/articles/progressive-networking-serviceworker
-        */
+                           Read more:
+                           https://ponyfoo.com/articles/progressive-networking-serviceworker
+                        */
         var networked = fetch(event.request)
           // We handle the network request with success and failure scenarios.
           .then(fetchedFromNetwork, unableToResolve)
@@ -130,31 +142,41 @@ self.addEventListener("fetch", function(event) {
           .catch(unableToResolve);
 
         /* We return the cached response immediately if there is one, and fall
-           back to waiting on the network as usual.
-        */
-        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+                           back to waiting on the network as usual.
+                        */
+        console.log(
+          "WORKER: fetch event",
+          cached ? "(cached)" : "(network)",
+          event.request.url
+        );
         return cached || networked;
 
         function fetchedFromNetwork(response) {
           /* We copy the response before replying to the network request.
-             This is the response that will be stored on the ServiceWorker cache.
-          */
+                               This is the response that will be stored on the ServiceWorker cache.
+                            */
           var cacheCopy = response.clone();
 
-          console.log('WORKER: fetch response from network.', event.request.url);
+          console.log(
+            "WORKER: fetch response from network.",
+            event.request.url
+          );
 
           caches
             // We open a cache to store the response for this request.
-            .open(version + 'pages')
+            .open(version + "pages")
             .then(function add(cache) {
               /* We store the response for this request. It'll later become
-                 available to caches.match(event.request) calls, when looking
-                 for cached responses.
-              */
+                                           available to caches.match(event.request) calls, when looking
+                                           for cached responses.
+                                        */
               cache.put(event.request, cacheCopy);
             })
             .then(function() {
-              console.log('WORKER: fetch response stored in cache.', event.request.url);
+              console.log(
+                "WORKER: fetch response stored in cache.",
+                event.request.url
+              );
             });
 
           // Return the response so that the promise is settled in fulfillment.
@@ -162,31 +184,33 @@ self.addEventListener("fetch", function(event) {
         }
 
         /* When this method is called, it means we were unable to produce a response
-           from either the cache or the network. This is our opportunity to produce
-           a meaningful response even when all else fails. It's the last chance, so
-           you probably want to display a "Service Unavailable" view or a generic
-           error response.
-        */
-        function unableToResolve () {
+                           from either the cache or the network. This is our opportunity to produce
+                           a meaningful response even when all else fails. It's the last chance, so
+                           you probably want to display a "Service Unavailable" view or a generic
+                           error response.
+                        */
+        function unableToResolve() {
           /* There's a couple of things we can do here.
-             - Test the Accept header and then return one of the `offlineFundamentals`
-               e.g: `return caches.match('/some/cached/image.png')`
-             - You should also consider the origin. It's easier to decide what
-               "unavailable" means for requests against your origins than for requests
-               against a third party, such as an ad provider.
-             - Generate a Response programmaticaly, as shown below, and return that.
-          */
+                               - Test the Accept header and then return one of the `offlineFundamentals`
+                                 e.g: `return caches.match('/some/cached/image.png')`
+                               - You should also consider the origin. It's easier to decide what
+                                 "unavailable" means for requests against your origins than for requests
+                                 against a third party, such as an ad provider.
+                               - Generate a Response programmaticaly, as shown below, and return that.
+                            */
 
-          console.log('WORKER: fetch request failed in both cache and network.');
+          console.log(
+            "WORKER: fetch request failed in both cache and network."
+          );
 
           /* Here we're creating a response programmatically. The first parameter is the
-             response body, and the second one defines the options for the response.
-          */
-          return new Response('<h1>Service Unavailable</h1>', {
+                               response body, and the second one defines the options for the response.
+                            */
+          return new Response("<h1>Service Unavailable</h1>", {
             status: 503,
-            statusText: 'Service Unavailable',
+            statusText: "Service Unavailable",
             headers: new Headers({
-              'Content-Type': 'text/html'
+              "Content-Type": "text/html"
             })
           });
         }
@@ -202,38 +226,38 @@ self.addEventListener("fetch", function(event) {
 */
 self.addEventListener("activate", function(event) {
   /* Just like with the install event, event.waitUntil blocks activate on a promise.
-     Activation will fail unless the promise is fulfilled.
-  */
-  console.log('WORKER: activate event in progress.');
+       Activation will fail unless the promise is fulfilled.
+    */
+  console.log("WORKER: activate event in progress.");
 
   event.waitUntil(
     caches
       /* This method returns a promise which will resolve to an array of available
-         cache keys.
-      */
+               cache keys.
+            */
       .keys()
-      .then(function (keys) {
+      .then(function(keys) {
         // We return a promise that settles when all outdated caches are deleted.
         return Promise.all(
           keys
-            .filter(function (key) {
+            .filter(function(key) {
               // Filter by keys that don't start with the latest version prefix.
               return !key.startsWith(version);
             })
-            .map(function (key) {
+            .map(function(key) {
               /* Return a promise that's fulfilled
-                 when each outdated cache is deleted.
-              */
+                                               when each outdated cache is deleted.
+                                            */
               return caches.delete(key);
             })
         );
-      }).then(() => self.clients.claim())
+      })
+      .then(() => self.clients.claim())
       .then(function() {
-        console.log('WORKER: activate completed.');
+        console.log("WORKER: activate completed.");
       })
   );
 });
-
 
 /*const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
