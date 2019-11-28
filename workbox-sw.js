60d05e334041ec611060c7fce731cb867ba8f838
arului <58032678+arului@users.noreply.github.com>
2019-11-28 11:42:57 +0000

    Update workbox-sw.js

--- workbox-sw.js
+++ workbox-sw.js
@@ -1,1 +1,12 @@
-console.log("todo")
+  if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker
+    .register('../service-worker.js', { scope: './' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+
+}
