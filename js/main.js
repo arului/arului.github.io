e324b684e546ef754c9c0fc9142b077c93905a8b
arului <58032678+arului@users.noreply.github.com>
2019-12-17 09:54:39 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,5 +56,90 @@ $(document).ready(function() {
     window.location = event.currentTarget.value;
     }
   });
+  pageSize = 2;
+  incremSlide = 3;
+  startPage = 0;
+  numberPage = 0;
+
+  var pageCount = $(".line-content").length / pageSize;
+  var totalSlidepPage = Math.floor(pageCount / incremSlide);
+
+  for (var i = 0; i < pageCount; i++) {
+    $("#pagin").append('<li><a href="#">' + (i + 1) + "</a></li> ");
+    if (i > pageSize) {
+      $("#pagin li")
+        .eq(i)
+        .hide();
+    }
+  }
+
+  var prev = $("<li/>")
+    .addClass("prev")
+    .html('<i class="fa fa-backward">&nbsp;Prev')
+    .click(function() {
+      startPage -= 3;
+      incremSlide -= 3;
+      numberPage--;
+      slide();
+    });
+
+  prev.hide();
+
+  var next = $("<li/>")
+    .addClass("next")
+    .html('Next&nbsp;<i class="fa fa-forward">')
+    .click(function() {
+      startPage += 3;
+      incremSlide += 3;
+      numberPage++;
+      slide();
+    });
+
+  $("#pagin")
+    .prepend(prev)
+    .append(next);
+
+  $("#pagin li")
+    .first()
+    .find("a")
+    .addClass("current");
+
+  slide = function(sens) {
+    $("#pagin li").hide();
+
+    for (t = startPage; t < incremSlide; t++) {
+      $("#pagin li")
+        .eq(t + 1)
+        .show();
+    }
+    if (startPage == 0) {
+      next.show();
+      prev.hide();
+    } else if (numberPage == totalSlidepPage) {
+      next.hide();
+      prev.show();
+    } else {
+      next.show();
+      prev.show();
+    }
+  };
+
+  showPage = function(page) {
+    $(".line-content").hide();
+    $(".line-content").each(function(n) {
+      if (n >= pageSize * (page - 1) && n < pageSize * page) $(this).show();
+    });
+  };
+
+  showPage(1);
+  $("#pagin li a")
+    .eq(0)
+    .addClass("current");
+
+  $("#pagin li a").click(function() {
+    $("#pagin li a").removeClass("current");
+    $(this).addClass("current");
+    showPage(parseInt($(this).text()));
+  });
 });
 
